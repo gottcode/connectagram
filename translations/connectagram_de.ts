b462802343b8da32fe07c96135949cb59407f70f
Graeme Gott <graeme@gottcode.org>
2013-05-05 05:25:15 -0400

    Sync translations with Transifex.

---
+++ translations/connectagram_de.ts
@@ -0,0 +1,402 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name>ChainPattern</name>
+    <message>
+        <location filename="../src/pattern.h" line="143"/>
+        <source>Chain</source>
+        <translation>Kette </translation>
+    </message>
+</context>
+<context>
+    <name>Clock</name>
+    <message>
+        <location filename="../src/clock.cpp" line="70"/>
+        <source>Loading</source>
+        <translation>Wird geladen</translation>
+    </message>
+    <message>
+        <location filename="../src/clock.cpp" line="112"/>
+        <source>Paused</source>
+        <translation>Pausiert</translation>
+    </message>
+</context>
+<context>
+    <name>Definitions</name>
+    <message>
+        <location filename="../src/definitions.cpp" line="36"/>
+        <source>Definitions</source>
+        <translation>Definitionen</translation>
+    </message>
+    <message>
+        <location filename="../src/definitions.cpp" line="145"/>
+        <source>Downloading definition...</source>
+        <translation>Definition herunterladen...</translation>
+    </message>
+    <message>
+        <location filename="../src/definitions.cpp" line="153"/>
+        <source>Unsolved word</source>
+        <translation>Ungelöstes Wort </translation>
+    </message>
+</context>
+<context>
+    <name>Dictionary</name>
+    <message>
+        <location filename="../src/dictionary.cpp" line="148"/>
+        <source>Definition from Wiktionary, the free dictionary</source>
+        <translation>Definition aus Wktionary, dem freien Wörterbuch </translation>
+    </message>
+    <message>
+        <location filename="../src/dictionary.cpp" line="155"/>
+        <source>No definition found</source>
+        <translation>Keine Definition gefunden</translation>
+    </message>
+    <message>
+        <location filename="../src/dictionary.cpp" line="158"/>
+        <source>Unable to connect to Wiktionary</source>
+        <translation> Verbindung zum Wiktionary kann nicht hergestellt werden </translation>
+    </message>
+</context>
+<context>
+    <name>FencePattern</name>
+    <message>
+        <location filename="../src/pattern.h" line="168"/>
+        <source>Fence</source>
+        <translation>Hindernis </translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="50"/>
+        <source>Select application language:</source>
+        <translation>Programmsprache auswählen:</translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="53"/>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;System Sprache&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="184"/>
+        <source>Note</source>
+        <translation>Notiz</translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="184"/>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Starten Sie das Programm neu damit die Sprachänderungen übernommen werden können.</translation>
+    </message>
+</context>
+<context>
+    <name>NewGameDialog</name>
+    <message>
+        <location filename="../src/new_game_dialog.cpp" line="45"/>
+        <source>New Game</source>
+        <translation>Neues Spiel</translation>
+    </message>
+    <message>
+        <location filename="../src/new_game_dialog.cpp" line="58"/>
+        <location filename="../src/window.cpp" line="266"/>
+        <source>Low</source>
+        <translation>Niedrig </translation>
+    </message>
+    <message>
+        <location filename="../src/new_game_dialog.cpp" line="58"/>
+        <location filename="../src/window.cpp" line="267"/>
+        <source>Medium</source>
+        <translation>Mittel</translation>
+    </message>
+    <message>
+        <location filename="../src/new_game_dialog.cpp" line="58"/>
+        <location filename="../src/window.cpp" line="268"/>
+        <source>High</source>
+        <translation>Hoch</translation>
+    </message>
+    <message>
+        <location filename="../src/new_game_dialog.cpp" line="58"/>
+        <location filename="../src/window.cpp" line="269"/>
+        <source>Very High</source>
+        <translation>Sehr hoch</translation>
+    </message>
+    <message>
+        <location filename="../src/new_game_dialog.cpp" line="93"/>
+        <location filename="../src/window.cpp" line="280"/>
+        <source>Language:</source>
+        <translation>Sprache:</translation>
+    </message>
+    <message>
+        <location filename="../src/new_game_dialog.cpp" line="95"/>
+        <location filename="../src/window.cpp" line="282"/>
+        <source>Word Length:</source>
+        <translation>Wortlänge:</translation>
+    </message>
+    <message>
+        <location filename="../src/new_game_dialog.cpp" line="94"/>
+        <location filename="../src/window.cpp" line="281"/>
+        <source>Amount of Words:</source>
+        <translation>Anzahl der Wörter:</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/new_game_dialog.cpp" line="143"/>
+        <location filename="../src/window.cpp" line="282"/>
+        <source>%n letter(s)</source>
+        <translation>
+            <numerusform>%n buchstaben</numerusform>
+            <numerusform>%n buchstaben</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>RingsPattern</name>
+    <message>
+        <location filename="../src/pattern.h" line="193"/>
+        <source>Rings</source>
+        <translation>Ringe</translation>
+    </message>
+</context>
+<context>
+    <name>ScoreBoard</name>
+    <message>
+        <location filename="../src/score_board.cpp" line="31"/>
+        <source>Scores</source>
+        <translation>Spielergebnisse </translation>
+    </message>
+    <message>
+        <location filename="../src/score_board.cpp" line="35"/>
+        <source>Score</source>
+        <translation>Spielergebniss</translation>
+    </message>
+    <message>
+        <location filename="../src/score_board.cpp" line="35"/>
+        <source>Time</source>
+        <translation>Zeit</translation>
+    </message>
+    <message>
+        <location filename="../src/score_board.cpp" line="35"/>
+        <source>Words</source>
+        <translation>Wörter</translation>
+    </message>
+    <message>
+        <location filename="../src/score_board.cpp" line="35"/>
+        <source>Length</source>
+        <translation>Länge</translation>
+    </message>
+</context>
+<context>
+    <name>StairsPattern</name>
+    <message>
+        <location filename="../src/pattern.h" line="214"/>
+        <source>Stairs</source>
+        <translation>Sterne</translation>
+    </message>
+</context>
+<context>
+    <name>TwistyPattern</name>
+    <message>
+        <location filename="../src/pattern.h" line="231"/>
+        <source>Twisty</source>
+        <translation>Kurvenreich </translation>
+    </message>
+</context>
+<context>
+    <name>WavePattern</name>
+    <message>
+        <location filename="../src/pattern.h" line="254"/>
+        <source>Wave</source>
+        <translation>Welle </translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="../src/window.cpp" line="69"/>
+        <location filename="../src/window.cpp" line="84"/>
+        <source>Success</source>
+        <translation>Erfolg </translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="96"/>
+        <source>Loading</source>
+        <translation>Wird geladen</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="96"/>
+        <source>Paused</source>
+        <translation>Pausiert</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="119"/>
+        <source>Definitions</source>
+        <translation>Definitionen</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="125"/>
+        <source>Hint</source>
+        <translation>Hinweis </translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="156"/>
+        <source>&amp;Game</source>
+        <translation>Spiel</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="157"/>
+        <source>&amp;New</source>
+        <translation>Neu</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="157"/>
+        <source>Ctrl+N</source>
+        <translation>Strg+N</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="158"/>
+        <source>&amp;Choose...</source>
+        <translation>Auswählen...</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="167"/>
+        <source>&amp;Details</source>
+        <translation>Details</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="160"/>
+        <location filename="../src/window.cpp" line="326"/>
+        <source>&amp;Pause</source>
+        <translation>Pause</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="160"/>
+        <source>P</source>
+        <translation>P</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="162"/>
+        <source>&amp;Hint</source>
+        <translation>Hinweis </translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="162"/>
+        <source>H</source>
+        <translation>H</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="165"/>
+        <source>D&amp;efinitions</source>
+        <translation>Definitionen</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="165"/>
+        <source>D</source>
+        <translation>D</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="168"/>
+        <source>&amp;Scores</source>
+        <translation>Spielergebnisse</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="170"/>
+        <source>&amp;Quit</source>
+        <translation>Schliessen</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="170"/>
+        <source>Ctrl+Q</source>
+        <translation>Strg+Q</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="172"/>
+        <source>&amp;Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="173"/>
+        <source>Application &amp;Language...</source>
+        <translation>Programm &amp; Sprache...</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="175"/>
+        <source>&amp;Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="176"/>
+        <source>&amp;About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="177"/>
+        <source>About &amp;Qt</source>
+        <translation>Über Qt</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="205"/>
+        <source>Choose Game</source>
+        <translation>Spiel auswählen</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="205"/>
+        <location filename="../src/window.cpp" line="283"/>
+        <source>Game Number:</source>
+        <translation>Spielnummer:</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="209"/>
+        <source>Sorry</source>
+        <translation>Entschuldigung</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="209"/>
+        <source>Unable to start requested game.</source>
+        <translation>Das angeforderte Spiel kann nicht gestartet werden. </translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="249"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="250"/>
+        <source>Connectagram</source>
+        <translation>Connectagram</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="251"/>
+        <source>A word unscrambling game</source>
+        <translation>Ein Wort unscrambling Spiel </translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="252"/>
+        <source>Copyright &amp;copy; 2009-%1 by Graeme Gott</source>
+        <translation>Copyright &amp;copy; 2009-%1 bei Graeme Gott</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="253"/>
+        <source>Released under the &lt;a href=&quot;http://www.gnu.org/licenses/gpl.html&quot;&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>Released under the &lt;a href=&quot;http://www.gnu.org/licenses/gpl.html&quot;&gt;GPL 3&lt;/a&gt; license</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="254"/>
+        <source>Definitions are from &lt;a href=&quot;http://wiktionary.org/&quot;&gt;Wiktionary&lt;/a&gt;</source>
+        <translation>Definitionen sind von &lt;a href=&quot;http://wiktionary.org/&quot;&gt;Wiktionary&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="277"/>
+        <source>Details</source>
+        <translation>Details</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="279"/>
+        <source>Pattern:</source>
+        <translation>Muster: </translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="326"/>
+        <source>&amp;Resume</source>
+        <translation>Fortsetzen </translation>
+    </message>
+</context>
+</TS>
