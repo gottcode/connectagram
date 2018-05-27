7012882b782412ba3778c0ba420565e7f8ed8279
Graeme Gott <graeme@gottcode.org>
2018-05-27 16:21:58 -0400

    Add translations to app data and desktop file at compile time.

--- icons/po/update-po.sh
+++ icons/po/update-po.sh
@@ -4,63 +4,45 @@
 echo -n 'Preparing files...'
 cd ..
 
-rm -f connectagram.desktop.in.h
-rm -f connectagram.desktop.in
-cp connectagram.desktop connectagram.desktop.in
-sed -e '/^Name\[/ d' \
-	-e '/^GenericName\[/ d' \
-	-e '/^Comment\[/ d' \
-	-e 's/^Name/_Name/' \
-	-e 's/^GenericName/_GenericName/' \
-	-e 's/^Comment/_Comment/' \
-	-i connectagram.desktop.in
-intltool-extract --quiet --type=gettext/ini connectagram.desktop.in
+rm -f connectagram.desktop.in.bak
+cp connectagram.desktop.in connectagram.desktop.in.bak
+sed -e '/^Icon/ d' -e '/^Keywords/ d' -i connectagram.desktop.in
 
-rm -f connectagram.appdata.xml.in.h
-rm -f connectagram.appdata.xml.in
-cp connectagram.appdata.xml connectagram.appdata.xml.in
-sed -e '/p xml:lang/ d' \
-	-e '/summary xml:lang/ d' \
-	-e '/name xml:lang/ d' \
-	-e 's/<p>/<_p>/' \
-	-e 's/<\/p>/<\/_p>/' \
-	-e 's/<summary>/<_summary>/' \
-	-e 's/<\/summary>/<\/_summary>/' \
-	-e 's/<name>/<_name>/' \
-	-e 's/<\/name>/<\/_name>/' \
-	-i connectagram.appdata.xml.in
-intltool-extract --quiet --type=gettext/xml connectagram.appdata.xml.in
+rm -f connectagram.appdata.xml.in.bak
+cp connectagram.appdata.xml.in connectagram.appdata.xml.in.bak
+sed '/<developer_name>/ d' -i connectagram.appdata.xml.in
 
 cd po
 echo ' DONE'
 
 
 echo -n 'Extracting messages...'
-xgettext --from-code=UTF-8 --c++ --keyword=_ --keyword=N_:1 \
+xgettext --from-code=UTF-8 --output=description.pot \
 	--package-name='Connectagram' --copyright-holder='Graeme Gott' \
-	--output=description.pot ../*.h
+	../*.in
 sed 's/CHARSET/UTF-8/' -i description.pot
 echo ' DONE'
 
 
 echo -n 'Updating translations...'
+rm -f LINGUAS
 for POFILE in *.po;
 do
 	echo -n " $POFILE"
+	echo -n " ${POFILE%???}" >> LINGUAS
 	msgmerge --quiet --update --backup=none $POFILE description.pot
 done
+echo >> LINGUAS
 echo ' DONE'
 
 
-echo -n 'Merging translations...'
+echo -n 'Cleaning up...'
 cd ..
 
-intltool-merge --quiet --desktop-style po connectagram.desktop.in connectagram.desktop
-rm -f connectagram.desktop.in.h
 rm -f connectagram.desktop.in
+mv connectagram.desktop.in.bak connectagram.desktop.in
 
-intltool-merge --quiet --xml-style po connectagram.appdata.xml.in connectagram.appdata.xml
-rm -f connectagram.appdata.xml.in.h
 rm -f connectagram.appdata.xml.in
+mv connectagram.appdata.xml.in.bak connectagram.appdata.xml.in
 
 echo ' DONE'
