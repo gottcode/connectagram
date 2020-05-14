83cb86f0f8e4a9e9419bf2a456652e0a6fccf2c3
mazbrili <mazbrili@yahoo.com>
2020-05-14 15:30:41 +0700

    Create connectagram_id.ts
    add indonesian translation

---
+++ translations/connectagram_id.ts
@@ -0,0 +1,315 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>ChainPattern</name>
+    <message>
+        <source>Chain</source>
+        <translation>Rantai</translation>
+    </message>
+</context>
+<context>
+    <name>Clock</name>
+    <message>
+        <source>Loading</source>
+        <translation>Memuat</translation>
+    </message>
+    <message>
+        <source>Paused</source>
+        <translation>Berhenti</translation>
+    </message>
+</context>
+<context>
+    <name>Definitions</name>
+    <message>
+        <source>Definitions</source>
+        <translation>Definisi</translation>
+    </message>
+    <message>
+        <source>Downloading definition...</source>
+        <translation>Mengunduh definisi...</translation>
+    </message>
+    <message>
+        <source>Unsolved word</source>
+        <translation>Perkataan belum selesai</translation>
+    </message>
+</context>
+<context>
+    <name>Dictionary</name>
+    <message>
+        <source>Definition from Wiktionary, the free dictionary</source>
+        <translation>Definisi dari Witionary, Kamus bebas</translation>
+    </message>
+    <message>
+        <source>No definition found</source>
+        <translation>Keterangan tidak ditemukan</translation>
+    </message>
+    <message>
+        <source>Unable to connect to Wiktionary</source>
+        <translation>Tidak bisa terhubung ke Wiktionary</translation>
+    </message>
+</context>
+<context>
+    <name>FencePattern</name>
+    <message>
+        <source>Fence</source>
+        <translation>Pagar</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>Pilih bahasa aplikasi:</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;Bahasa system&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>Catatan</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Silakan restart aplikasi ini agar perubahan dalam bahasa berlaku.</translation>
+    </message>
+</context>
+<context>
+    <name>NewGameDialog</name>
+    <message>
+        <source>New Game</source>
+        <translation>Permainan baru</translation>
+    </message>
+    <message>
+        <source>Low</source>
+        <translation>Rendah</translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation>Menengah</translation>
+    </message>
+    <message>
+        <source>High</source>
+        <translation>Tinggi</translation>
+    </message>
+    <message>
+        <source>Very High</source>
+        <translation>Sangat tinggi</translation>
+    </message>
+    <message>
+        <source>Language:</source>
+        <translation>Bahasa:</translation>
+    </message>
+    <message>
+        <source>Word Length:</source>
+        <translation>Panjang kata:</translation>
+    </message>
+    <message>
+        <source>Amount of Words:</source>
+        <translation>Jumlah kata:</translation>
+    </message>
+    <message numerus="yes">
+        <source>%n letter(s)</source>
+        <translation>
+            <numerusform>%n surat</numerusform>
+            <numerusform>%n surat-surat</numerusform>
+            <numerusform>%n surat</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>RingsPattern</name>
+    <message>
+        <source>Rings</source>
+        <translation>Dering</translation>
+    </message>
+</context>
+<context>
+    <name>ScoreBoard</name>
+    <message>
+        <source>Scores</source>
+        <translation>Nilai</translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation>Nilai</translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translation>Waktu</translation>
+    </message>
+    <message>
+        <source>Words</source>
+        <translation>Kata</translation>
+    </message>
+    <message>
+        <source>Length</source>
+        <translation>Panjang</translation>
+    </message>
+</context>
+<context>
+    <name>StairsPattern</name>
+    <message>
+        <source>Stairs</source>
+        <translation>Tangga</translation>
+    </message>
+</context>
+<context>
+    <name>TwistyPattern</name>
+    <message>
+        <source>Twisty</source>
+        <translation>Belokan</translation>
+    </message>
+</context>
+<context>
+    <name>WavePattern</name>
+    <message>
+        <source>Wave</source>
+        <translation>Gelombang</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>Success</source>
+        <translation>Berhasil</translation>
+    </message>
+    <message>
+        <source>Loading</source>
+        <translation>Memuat</translation>
+    </message>
+    <message>
+        <source>Paused</source>
+        <translation>Dijeda</translation>
+    </message>
+    <message>
+        <source>Definitions</source>
+        <translation>Definisi</translation>
+    </message>
+    <message>
+        <source>Hint</source>
+        <translation>Petunjuk</translation>
+    </message>
+    <message>
+        <source>&amp;Game</source>
+        <translation>&amp;Permainan</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Baru</translation>
+    </message>
+    <message>
+        <source>&amp;Choose...</source>
+        <translation>&amp;Memilih...</translation>
+    </message>
+    <message>
+        <source>&amp;Details</source>
+        <translation>&amp;Detil</translation>
+    </message>
+    <message>
+        <source>&amp;Pause</source>
+        <translation>&amp;Jeda</translation>
+    </message>
+    <message>
+        <source>P</source>
+        <translation>P</translation>
+    </message>
+    <message>
+        <source>&amp;Hint</source>
+        <translation>&amp;Petunjuk</translation>
+    </message>
+    <message>
+        <source>H</source>
+        <translation>H</translation>
+    </message>
+    <message>
+        <source>D&amp;efinitions</source>
+        <translation>D&amp;efinisi</translation>
+    </message>
+    <message>
+        <source>D</source>
+        <translation>D</translation>
+    </message>
+    <message>
+        <source>&amp;Scores</source>
+        <translation>&amp;Nilai</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Keluar</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;Pengaturan</translation>
+    </message>
+    <message>
+        <source>Application &amp;Language...</source>
+        <translation>&amp;Bahasa Aplikasi</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Tolong</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;Tentang</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>Tentang &amp;Qt</translation>
+    </message>
+    <message>
+        <source>Choose Game</source>
+        <translation>Memilih permainan</translation>
+    </message>
+    <message>
+        <source>Game Number:</source>
+        <translation>Jumlah Permainan:</translation>
+    </message>
+    <message>
+        <source>Sorry</source>
+        <translation>Maaf</translation>
+    </message>
+    <message>
+        <source>Unable to start requested game.</source>
+        <translation>Tidak bisa memulai permainan yang diminta.</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Tentang</translation>
+    </message>
+    <message>
+        <source>Connectagram</source>
+        <translation>Connectagram</translation>
+    </message>
+    <message>
+        <source>A word unscrambling game</source>
+        <translation>Permainan mencocokkan kata</translation>
+    </message>
+    <message>
+        <source>Copyright &amp;copy; 2009-%1 by Graeme Gott</source>
+        <translation>Hakcipta &amp;copy; 2009-%1, Graeme Gott</translation>
+    </message>
+    <message>
+        <source>Released under the &lt;a href=&quot;http://www.gnu.org/licenses/gpl.html&quot;&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>Dirilis dibawah &lt;a href=&quot;http://www.gnu.org/licenses/gpl.html&quot;&gt;GPL 3&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>Definitions are from &lt;a href=&quot;http://wiktionary.org/&quot;&gt;Wiktionary&lt;/a&gt;</source>
+        <translation>Definisi dari  &lt;a href=&quot;http://wiktionary.org/&quot;&gt;Wiktionary&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>Details</source>
+        <translation>Detil</translation>
+    </message>
+    <message>
+        <source>Pattern:</source>
+        <translation>Pola:</translation>
+    </message>
+    <message>
+        <source>&amp;Resume</source>
+        <translation>&amp;Lanjutkan</translation>
+    </message>
+</context>
+</TS>
