b68a64a426d31c8d0b3884a24c7ff08f6a67589a
Graeme Gott <graeme@gottcode.org>
2024-07-07 01:49:55 -0400

    Update translation language to be en

--- translations/connectagram_hu.ts
+++ translations/connectagram_hu.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="hu">
+<TS version="2.1" language="hu" sourcelanguage="en">
 <context>
     <name>ChainPattern</name>
     <message>
